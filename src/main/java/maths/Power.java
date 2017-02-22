aed54b66ca8baa500da14c9ef4e44d725c0104bd
Gabriel Massote <gabrielmassote@gmail.com>
2017-02-22 09:34:52 -0300

    feat(maths): implements algorithm to resolve power of numbers

--- src/main/java/maths/Power.java
+++ src/main/java/maths/Power.java
@@ -2,18 +2,18 @@ package maths;
 
 public class Power {
 
-	public static double powIterative(final double base, final int exponential) {
-		double result = base;
+	public static double powIterative(final double base, final int exponent) {
+		double result = 1;
 		
-		for (int i=1; i<exponential; i++)
+		for (int i=0; i<exponent; i++)
 			result *= base;
 		
 		return result;
 	}
 
-	public static double powRecursive(final double base, final int exponential) {
-		if (exponential == 1) return base;
-		return base*Power.powRecursive(base,  exponential-1);
+	public static double powRecursive(final double base, final int exponent) {
+		if (exponent == 1) return base;
+		return base*Power.powRecursive(base,  exponent-1);
 	}
 	
 }
